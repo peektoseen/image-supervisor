27b29b5a4a6deeb175948fca83b6c10df28e44d6
Nikolay Chuchadeev <peektoseen@gmail.com>
2023-11-17 11:29:57 +0300

    assets

---
+++ assets/AppAsset.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = ['css/site.css'];
+    public $js = ['js/main.js'];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset'
+    ];
+}
