1f4566c5bcdb334694a27d0525d999374e0789ed
Nikolay Chuchadeev <peektoseen@gmail.com>
2023-11-17 11:32:41 +0300

    move app to root

--- tests/acceptance/AboutCest.php
+++ tests/acceptance/AboutCest.php
@@ -4,7 +4,7 @@ use yii\helpers\Url;
 
 class AboutCest
 {
-    public function ensureThatAboutWorks(AcceptanceTester $I)
+    public function ensureThatAboutWorks(AcceptanceTester $I): void
     {
         $I->amOnPage(Url::toRoute('/site/about'));
         $I->see('About', 'h1');
