0239fd40c0a51dbe75c898596ace3c09dacd7f39
Nikolay Chuchadeev <peektoseen@gmail.com>
2023-11-17 11:32:18 +0300

    image model

---
+++ models/Image.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "image".
+ *
+ * @property int $id - primary
+ * @property int $source_id - used for generate id for next image via SQL without all records loop
+ * @property bool $approved - approved flag
+ * @property string $url - image url
+ * @property string $source - type of source (it may be several sources for images)
+ */
+class Image extends ActiveRecord
+{
+
+    const APPROVED = 'approved';
+    const DENIED = 'denied';
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName(): string
+    {
+        return 'images';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules(): array
+    {
+        return [
+            [['url', 'source_id', 'source'], 'required'],
+            [['url', 'source'], 'string'],
+            [['source_id', 'source'], 'unique', 'targetAttribute' => ['source_id', 'source']],
+            [['source_id'], 'integer'],
+            [['approved'], 'boolean']
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => 'ID',
+        ];
+    }
+}
