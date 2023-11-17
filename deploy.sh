3302b8bd2a6c4d67f04a6a0472d18754a28eb54f
Nikolay Chuchadeev <peektoseen@gmail.com>
2023-11-17 16:52:01 +0300

    bugfix for root user

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,15 @@
 echo "Generate .env file..."
 rm .env $2 > /dev/null
 cp .env.example .env
-echo "PHP_USER_ID=$(id -u)" >> .env
-echo "PHP_USER_GID=$(id -g)" >> .env
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "PHP_USER_ID=33" >> .env
+    echo "PHP_USER_GID=33" >> .env
+else
+    echo "PHP_USER_ID=$(id -u)" >> .env
+    echo "PHP_USER_GID=$(id -g)" >> .env
+fi
+
 length=16
 POSTGRES_PASSWORD=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c "$length")
 echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env
@@ -13,7 +20,13 @@ COOKIE_VALIDATION_KEY=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c "$length")
 echo "COOKIE_VALIDATION_KEY=$COOKIE_VALIDATION_KEY" >> .env
 
 echo "Start docker-compose services..."
-docker-compose -f ./docker/docker-compose.yml  build  --build-arg EXT_PHP_USER_ID=$(id -u) --build-arg EXT_PHP_USER_GID=$(id -g)
+
+if [ "$(id -u)" -eq 0 ]; then
+  docker-compose -f ./docker/docker-compose.yml  build  --build-arg EXT_PHP_USER_ID=33 --build-arg EXT_PHP_USER_GID=33
+else
+  docker-compose -f ./docker/docker-compose.yml  build  --build-arg EXT_PHP_USER_ID=$(id -u) --build-arg EXT_PHP_USER_GID=$(id -g)
+fi
+
 docker-compose  -f  ./docker/docker-compose.yml up php-composer
 
 docker-compose -f ./docker/docker-compose.yml up -d
