7edbb853384aa94eb1556680d600149fa2f32f12
Nikolay Chuchadeev <peektoseen@gmail.com>
2023-11-17 14:11:29 +0300

    docker improvements

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Generate .env file..."
-rm .env
+rm .env $2 > /dev/null
 cp .env.example .env
-echo "WWW-USER=$(id -u)" >> .env
+echo "PHP_USER_ID=$(id -u)" >> .env
+echo "PHP_USER_GID=$(id -g)" >> .env
 length=16
 POSTGRES_PASSWORD=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c "$length")
 echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env
@@ -12,6 +13,9 @@ COOKIE_VALIDATION_KEY=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c "$length")
 echo "COOKIE_VALIDATION_KEY=$COOKIE_VALIDATION_KEY" >> .env
 
 echo "Start docker-compose services..."
+docker-compose -f ./docker/docker-compose.yml  build  --build-arg EXT_PHP_USER_ID=$(id -u) --build-arg EXT_PHP_USER_GID=$(id -g)
+docker-compose  -f  ./docker/docker-compose.yml up php-composer
+
 docker-compose -f ./docker/docker-compose.yml up -d
 #docker-compose exec php composer install
 
