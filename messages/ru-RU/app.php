23349a85198ce010e381622f140a1406cbd817da
Nikolay Chuchadeev <peektoseen@gmail.com>
2023-11-17 11:30:22 +0300

    i18n

---
+++ messages/ru-RU/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Approve' => 'Одобрить',
+    'Deny' => 'Отклонить',
+    'Approved' => 'Одобрена',
+    'Denied' => 'Отклонена',
+    'Delete' => 'Отмена решения',
+    'Status' => 'Решение',
+    'Error' => 'Ошибка',
+    'Please contact us if you think this is a server error. Thank you.' => 'Пожалуйста, свяжитесь с нами, если вы считаете что это серверная ошибка. Спасибо.',
+    'Back to main page' => 'Назад на главную',
+    'Image List' => 'Список изображений',
+    'Access denied. Invalid or missing token.' => 'Доступ запрещен. Неверный или отсутствующий токен.',
+];
